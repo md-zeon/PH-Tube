e40d97feb31f43e56fcf3f654ebd9f10cbbbf886
md-zeon <zeonrahaman5870@gmail.com>
2025-03-12 08:43:44 +0600

    Dynamically added video cards

--- script/index.js
+++ script/index.js
@@ -23,3 +23,40 @@ function displayCategories(categories) {
 }
 
 loadCategories();
+
+function loadVideos() {
+	fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+		.then((res) => res.json())
+		.then((data) => displayVideos(data.videos));
+}
+
+const displayVideos = (videos) => {
+	// console.log(videos);
+    const videoContainer = document.getElementById("video-container");
+
+    videos.forEach(video => {
+        // console.log(video);
+        const videoCard = document.createElement("div");
+
+        videoCard.innerHTML = `
+            <div class="card bg-base-100 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="${video.title}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        `;
+
+        videoContainer.append(videoCard);
+    });
+}
+
+loadVideos();
